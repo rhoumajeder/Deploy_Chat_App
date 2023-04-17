95240553ec0c8232cfb2f2d2458d40838698b8e4
rhoumajeder <41521900+rhoumajeder@users.noreply.github.com>
2023-04-17 15:38:19 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,6 @@
-# VPC
+# This code defines infrastructure resources for deploying a Rocket-app VPC with security group, subnet, and EC2 instances in AWS using Terraform
+
+# VPC resource
 resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr_block
 
@@ -7,10 +9,13 @@ resource "aws_vpc" "this" {
   }
 }
 
+# Security group resource
+
 resource "aws_security_group" "rockatapp" {
   name        = "rockatapp"
   description = "Allow SSH access"
-
+  
+ # Allow all incoming TCP traffic
   ingress {
     from_port   = 22
     to_port     = 22
@@ -24,6 +29,8 @@ resource "aws_security_group" "rockatapp" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  # Allow all outgoing TCP traffic
+  
    egress {
     from_port   = 0
     to_port     = 65535
@@ -47,12 +54,14 @@ resource "aws_security_group" "rockatapp" {
   
 }
 
+# EC2 instance resource
 resource "aws_instance" "rockatapp" {
   count = 3
   ami           = "ami-086df58ea1b1ad56a"
   instance_type = "t3.small"
   key_name      = "RjeKeys"
   
+  # Attach the security group to the instances
   vpc_security_group_ids = [aws_security_group.rockatapp.id]
   
     tags = {
@@ -64,7 +73,7 @@ resource "aws_instance" "rockatapp" {
 }
 
 
-# Subnet
+# Subnet resource
 resource "aws_subnet" "this" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.subnet_cidr_block
@@ -79,6 +88,8 @@ resource "aws_subnet" "this" {
 
 }
 
+# Output resource for instance IP addresses
+
  output "ip_address" {
   #value = aws_instance.rockatapp.public_ip
   value = {
@@ -91,6 +102,8 @@ resource "aws_subnet" "this" {
   
 
 }
+
+# DNS resources
 
 # data "aws_route53_zone" "rockatapp" {
 #   name         = "jedder.net."
